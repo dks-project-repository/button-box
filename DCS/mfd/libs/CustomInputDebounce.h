762dbe6be90a4f38c5bf10a5fcb971c313c13dc9
kyedlin <kyedlin@hotmail.com>
2022-03-25 02:26:23 -0700

    starting to build rotary handlers

--- DCS/mfd/libs/CustomInputDebounce.h
+++ DCS/mfd/libs/CustomInputDebounce.h
@@ -1,66 +1,66 @@
 #include <InputDebounce.h>
 
-namespace CustomInputDebounce
-{
-  // const unsigned long BUTTON_DEBOUNCE_DELAY 20 // [ms]
-  // class MyInputDebounce : InputDebounce
-  // {
-  // public:
-  //   MyInputDebounce(int8_t pinIn = -1, unsigned long debDelay = DEFAULT_INPUT_DEBOUNCE_DELAY, PinInMode pinInMode = PIM_INT_PULL_UP_RES, unsigned long pressedDuration = 0)
-  //       : InputDebounce(pinIn, debDelay, pinInMode, pressedDuration), _pinLED(-1)
-  //   {
-  //   }
-  //   virtual ~MyInputDebounce()
-  //   {
-  //   }
-  //   void setPinLED(int8_t pinLED)
-  //   {
-  //     _pinLED = pinLED;
-  //   }
+namespace CustomInputDebounce {
+// const unsigned long BUTTON_DEBOUNCE_DELAY 20 // [ms]
+// class MyInputDebounce : InputDebounce
+// {
+// public:
+//   MyInputDebounce(int8_t pinIn = -1, unsigned long debDelay = DEFAULT_INPUT_DEBOUNCE_DELAY,
+//   PinInMode pinInMode = PIM_INT_PULL_UP_RES, unsigned long pressedDuration = 0)
+//       : InputDebounce(pinIn, debDelay, pinInMode, pressedDuration), _pinLED(-1)
+//   {
+//   }
+//   virtual ~MyInputDebounce()
+//   {
+//   }
+//   void setPinLED(int8_t pinLED)
+//   {
+//     _pinLED = pinLED;
+//   }
 
-  // protected:
-  //   virtual void pressed()
-  //   {
-  //     // handle pressed state
-  //     if (_pinLED >= 0)
-  //     {
-  //       digitalWrite(_pinLED, HIGH); // turn the LED on
-  //     }
-  //     Serial.print("HIGH (pin: ");
-  //     Serial.print(getPinIn());
-  //     Serial.println(")");
-  //   }
-  //   virtual void released()
-  //   {
-  //     // handle released state
-  //     if (_pinLED >= 0)
-  //     {
-  //       digitalWrite(_pinLED, LOW); // turn the LED off
-  //     }
-  //     Serial.print("LOW (pin: ");
-  //     Serial.print(getPinIn());
-  //     Serial.println(")");
-  //   }
-  //   virtual void pressedDuration(unsigned long duration)
-  //   {
-  //     // handle still pressed state
-  //     Serial.print("HIGH (pin: ");
-  //     Serial.print(getPinIn());
-  //     Serial.print(") still pressed, duration ");
-  //     Serial.print(duration);
-  //     Serial.println("ms");
-  //   }
-  //   virtual void releasedDuration(unsigned long duration)
-  //   {
-  //     // handle released state
-  //     Serial.print("LOW (pin: ");
-  //     Serial.print(getPinIn());
-  //     Serial.print("), duration ");
-  //     Serial.print(duration);
-  //     Serial.println("ms");
-  //   }
+// protected:
+//   virtual void pressed()
+//   {
+//     // handle pressed state
+//     if (_pinLED >= 0)
+//     {
+//       digitalWrite(_pinLED, HIGH); // turn the LED on
+//     }
+//     Serial.print("HIGH (pin: ");
+//     Serial.print(getPinIn());
+//     Serial.println(")");
+//   }
+//   virtual void released()
+//   {
+//     // handle released state
+//     if (_pinLED >= 0)
+//     {
+//       digitalWrite(_pinLED, LOW); // turn the LED off
+//     }
+//     Serial.print("LOW (pin: ");
+//     Serial.print(getPinIn());
+//     Serial.println(")");
+//   }
+//   virtual void pressedDuration(unsigned long duration)
+//   {
+//     // handle still pressed state
+//     Serial.print("HIGH (pin: ");
+//     Serial.print(getPinIn());
+//     Serial.print(") still pressed, duration ");
+//     Serial.print(duration);
+//     Serial.println("ms");
+//   }
+//   virtual void releasedDuration(unsigned long duration)
+//   {
+//     // handle released state
+//     Serial.print("LOW (pin: ");
+//     Serial.print(getPinIn());
+//     Serial.print("), duration ");
+//     Serial.print(duration);
+//     Serial.println("ms");
+//   }
 
-  // private:
-  //   int8_t _pinLED;
-  // };
-}
+// private:
+//   int8_t _pinLED;
+// };
+}  // namespace CustomInputDebounce
--- DCS/mfd/mfd.ino
+++ DCS/mfd/mfd.ino
@@ -1,26 +1,41 @@
-#include "libs/CustomInputDebounce.h"
-#include "libs/DebouncEncoder.h"
 #include <Joystick.h>
 
-#define JOYSTICK_COUNT 1
+#include "libs/CustomInputDebounce.h"
+#include "libs/DebouncEncoder.h"
 
-Joystick_ Joystick[JOYSTICK_COUNT] = {
-  Joystick_(0x01, JOYSTICK_TYPE_JOYSTICK, 128, 0)
-};
+enum MFD {
+	Left,
+	Right,
+	Bottom,
+}
+
+static MFD = Right;
+
+Joystick_ Joystick(0x01, JOYSTICK_TYPE_JOYSTICK, 128, 0);
 
 CustomInputs::Encoder encoder1 = CustomInputs::Encoder(0, 1);
 CustomInputs::Encoder encoder2 = CustomInputs::Encoder(3, 4);
 
-void setup() { 
-  Serial.begin(115200); 
-  Joystick[0]
-  
-  }
+void handleRotaryInc(){
+	Joystick.pressButton(button);
+	delay(5);
+	Joystick.releaseButton(button);
+	delay(5);
+};
+
+
+
+void handleRotaryDec(){
+
+};
+
+void setup() {
+	Serial.begin(115200);
+	Joystick.begin();
+}
 
 void loop() {
-  unsigned long now = millis();
-  encoder1.rotary_tick();
-  encoder2.rotary_tick();
-
-  // delay(1); // [ms]
+	unsigned long now = millis();
+	encoder1.rotary_tick();
+	encoder2.rotary_tick();
 }
