83d1c21829d6eacb2015c4df0f2c081a0ce38e2e
kyedlin <kyedlin@hotmail.com>
2022-03-26 03:35:40 -0700

    working rotary

--- DCS/mfd/libs/DebouncEncoder.h
+++ DCS/mfd/libs/DebouncEncoder.h
@@ -1,64 +1,87 @@
+#include <Joystick.h>
+
 namespace CustomInputs {
 
-class Encoder {
-   private:
-	int8_t c;
-	int8_t val;
-	uint8_t _clk;
-	uint8_t _data;
-	uint8_t _position = 0;
-	uint8_t prevNextCode = 0;
-	uint16_t store = 0;
+enum StepsPerDetent { ONE_STEP_PER_DETENT = 1, TWO_STEPS_PER_DETENT = 2, FOUR_STEPS_PER_DETENT = 4 };
+
+class RotaryEncoder {
+ private:
+	Joystick_* joy;
+	uint8_t buttonInc_;
+	uint8_t buttonDec_;
+	// uint8_t val;
+	uint8_t pinClk_;
+	uint8_t pinData_;
+	uint8_t lastState_;
+	uint16_t delta_ = 0;
+
+	void momentaryPress(uint8_t button) {
+		joy->pressButton(button);
+		delay(25);
+		joy->releaseButton(button);
+		delay(25);
+	}
+
 	void init(uint8_t clk, uint8_t data) {
-		_clk = clk;
-		_data = data;
-		pinMode(_clk, INPUT);
-		pinMode(_clk, INPUT_PULLUP);
-		pinMode(_data, INPUT);
-		pinMode(_data, INPUT_PULLUP);
+		// void (*callback_)(char state, char* msg);
+		pinClk_ = clk;
+		pinData_ = data;
+		pinMode(pinClk_, INPUT_PULLUP);
+		pinMode(pinData_, INPUT_PULLUP);
+	}
+
+	void set_buttons(uint8_t buttonInc, uint8_t buttonDec) {
+		buttonInc_ = buttonInc;
+		buttonDec_ = buttonDec;
 	}
 
 	// A vald CW or  CCW move returns 1, invalid returns 0.
 	int8_t read_rotary() {
 		static int8_t rot_enc_table[] = {0, 1, 1, 0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 1, 0};
 
-		prevNextCode <<= 2;
-		if (digitalRead(_data)) prevNextCode |= 0x02;
-		if (digitalRead(_clk)) prevNextCode |= 0x01;
-		prevNextCode &= 0x0f;
+		lastState_ <<= 2;
+		if (digitalRead(pinData_)) lastState_ |= 0x02;
+		if (digitalRead(pinClk_)) lastState_ |= 0x01;
+		lastState_ &= 0x0f;
 
 		// If valid then store as 16 bit data.
-		if (rot_enc_table[prevNextCode]) {
-			store <<= 4;
-			store |= prevNextCode;
+		if (rot_enc_table[lastState_]) {
+			delta_ <<= 4;
+			delta_ |= lastState_;
 			// if (store==0xd42b) return 1;
 			// if (store==0xe817) return -1;
-			if ((store & 0xff) == 0x2b) return -1;
-			if ((store & 0xff) == 0x17) return 1;
+			if ((delta_ & 0xff) == 0x2b) return -1;
+			if ((delta_ & 0xff) == 0x17) return 1;
 		}
 		return 0;
 	}
-	void tick() {
-		if (val = read_rotary()) {
-			c += val;
-			Serial.print(c);
-			Serial.print(" ");
-
-			if (prevNextCode == 0x0b) {
+	void rotary_tick() {
+		if (read_rotary()) {
+			if (lastState_ == 0x0b) {
+				momentaryPress(buttonInc_);
 				Serial.print("eleven ");
-				Serial.println(store, HEX);
+				Serial.println(delta_, HEX);
 			}
 
-			if (prevNextCode == 0x07) {
+			if (lastState_ == 0x07) {
+				momentaryPress(buttonDec_);
 				Serial.print("seven ");
-				Serial.println(store, HEX);
+				Serial.println(delta_, HEX);
 			}
 		}
 	}
 
-   public:
-	Encoder(uint8_t clk, uint8_t data) { init(clk, data); };
-	void rotary_tick() { tick(); }
+ public:
+	void pollInput() { rotary_tick(); }
+	void setButtons(uint8_t buttonInc, uint8_t buttonDec) { set_buttons(buttonInc, buttonDec); }
+	RotaryEncoder(char pinClk, char pinData, Joystick_& joystick, char buttonInc, char buttonDec) {
+		joy = &joystick;
+		pinClk_ = pinClk;
+		pinData_ = pinData;
+		pinMode(pinClk_, INPUT_PULLUP);
+		pinMode(pinData_, INPUT_PULLUP);
+		set_buttons(buttonInc, buttonDec);
+		read_rotary();
+	};
 };
-
-}  // namespace CustomInputs
+}	 // namespace CustomInputs
--- DCS/mfd/mfd.ino
+++ DCS/mfd/mfd.ino
@@ -3,39 +3,18 @@
 #include "libs/CustomInputDebounce.h"
 #include "libs/DebouncEncoder.h"
 
-enum MFD {
-	Left,
-	Right,
-	Bottom,
-}
+static Joystick_ joy(0x03, JOYSTICK_TYPE_JOYSTICK, 128, 0);
 
-static MFD = Right;
-
-Joystick_ Joystick(0x01, JOYSTICK_TYPE_JOYSTICK, 128, 0);
-
-CustomInputs::Encoder encoder1 = CustomInputs::Encoder(0, 1);
-CustomInputs::Encoder encoder2 = CustomInputs::Encoder(3, 4);
-
-void handleRotaryInc(){
-	Joystick.pressButton(button);
-	delay(5);
-	Joystick.releaseButton(button);
-	delay(5);
-};
-
-
-
-void handleRotaryDec(){
-
-};
+CustomInputs::RotaryEncoder encoder1(0, 1, joy, 0, 1);
+CustomInputs::RotaryEncoder encoder2(3, 4, joy, 2, 3);
 
 void setup() {
 	Serial.begin(115200);
-	Joystick.begin();
+	joy.begin();
 }
 
 void loop() {
 	unsigned long now = millis();
-	encoder1.rotary_tick();
-	encoder2.rotary_tick();
+	encoder1.pollInput();
+	encoder2.pollInput();
 }
