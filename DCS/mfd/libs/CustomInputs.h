3846d006cc92dbf76df132bb4f833dc22e38d46b
kyedlin <kyedlin@hotmail.com>
2022-03-27 14:04:21 -0700

    working multi-joystick

---
+++ DCS/mfd/libs/CustomInputs.h
@@ -0,0 +1,131 @@
+#include <InputDebounce.h>
+
+namespace CustomInputs {
+
+enum StepsPerDetent {
+	ONE_STEP_PER_DETENT = 1,
+	TWO_STEPS_PER_DETENT = 2,
+	FOUR_STEPS_PER_DETENT = 4
+};
+
+class RotaryEncoder {
+ private:
+	// Joystick_* joy;
+	uint8_t buttonInc_;
+	uint8_t buttonDec_;
+	// uint8_t val;
+	uint8_t pinClk_;
+	uint8_t pinData_;
+	uint8_t lastState_;
+	uint16_t delta_ = 0;
+
+	void (*callback_)(uint8_t button);
+
+	void set_buttons(uint8_t buttonInc, uint8_t buttonDec) {
+		buttonInc_ = buttonInc;
+		buttonDec_ = buttonDec;
+	}
+
+	// A vald CW or  CCW move returns 1, invalid returns 0.
+	int8_t read_rotary() {
+		static int8_t rot_enc_table[] = {0, 1, 1, 0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 1, 0};
+
+		lastState_ <<= 2;
+		if (digitalRead(pinData_)) lastState_ |= 0x02;
+		if (digitalRead(pinClk_)) lastState_ |= 0x01;
+		lastState_ &= 0x0f;
+
+		// If valid then store as 16 bit data.
+		if (rot_enc_table[lastState_]) {
+			delta_ <<= 4;
+			delta_ |= lastState_;
+			// if (store==0xd42b) return 1;
+			// if (store==0xe817) return -1;
+			if ((delta_ & 0xff) == 0x2b) return -1;
+			if ((delta_ & 0xff) == 0x17) return 1;
+		}
+		return 0;
+	}
+	void rotary_tick() {
+		if (read_rotary()) {
+			if (lastState_ == 0x0b) {
+				callback_(buttonInc_);
+				// Serial.print("eleven ");
+				// Serial.println(delta_, HEX);
+			}
+
+			if (lastState_ == 0x07) {
+				callback_(buttonDec_);
+				// Serial.print("seven ");
+				// Serial.println(delta_, HEX);
+			}
+		}
+	}
+
+ public:
+	void pollInput() { rotary_tick(); }
+	void setButtons(uint8_t buttonInc, uint8_t buttonDec) {
+		set_buttons(buttonInc, buttonDec);
+	}
+	RotaryEncoder(uint8_t pinClk, uint8_t pinData, void (*callback)(uint8_t button), uint8_t buttonInc, uint8_t buttonDec) {
+		callback_ = callback;
+		pinClk_ = pinClk;
+		pinData_ = pinData;
+		pinMode(pinClk_, INPUT_PULLUP);
+		pinMode(pinData_, INPUT_PULLUP);
+		set_buttons(buttonInc, buttonDec);
+		read_rotary();
+	};
+};
+
+class Button : public InputDebounce {
+ private:
+	uint8_t button_;
+	void (*pressed_)(uint8_t button);
+	void (*released_)(uint8_t button);
+	void init(uint8_t pin, void (*pressed)(uint8_t button), void (*released)(uint8_t button), uint8_t button) {
+		button_ = button;
+		pressed_ = pressed;
+		released_ = released;
+	}
+	virtual void pressed() { pressed_(button_); }
+	virtual void released() { released_(button_); }
+
+ public:
+	void setButton(uint8_t button) { button_ = button; }
+	void setCallbacks(void (*pressed)(uint8_t button), void (*released)(uint8_t button)) {
+		pressed_ = pressed;
+		released_ = released;
+	}
+	Button(uint8_t pin, void (*pressed)(uint8_t button), void (*released)(uint8_t button), uint8_t button)
+			: InputDebounce(pin, 6, PIM_INT_PULL_UP_RES, 0) {
+		init(pin, pressed, released, button);
+	}
+	Button(const Button&);
+	Button& operator=(const Button&);
+	virtual ~Button() {}
+};
+
+// class MyInputDebounce : public InputDebounce {
+//  private:
+// 	Joystick_* joystick_;
+// 	char button_;
+
+//  protected:
+// 	virtual void pressed() { joystick_->pressButton(button_); }
+// 	virtual void released() { joystick_->releaseButton(button_); }
+
+//  public:
+// 	MyInputDebounce(int8_t pin, Joystick_& joystick, char button, unsigned long debDelay = DEFAULT_INPUT_DEBOUNCE_DELAY, PinInMode pinInMode = PIM_INT_PULL_UP_RES, unsigned long pressedDuration = 0)
+// 			: InputDebounce(pin, 6, pinInMode, pressedDuration) {
+// 		joystick_ = &joystick;
+// 		button_ = button;
+// 	}
+// 	MyInputDebounce(const MyInputDebounce&);
+//   	MyInputDebounce& operator=(const MyInputDebounce&);
+// 	virtual ~MyInputDebounce() {}
+// 	void setButton(char button) { button_ = button; }
+// 	void setJoystick(Joystick_& joystick) {joystick_ = &joystick;}
+// };
+
+}	 // namespace CustomInputs
--- DCS/mfd/mfd.ino
+++ DCS/mfd/mfd.ino
@@ -1,20 +1,82 @@
+#include <InputDebounce.h>
 #include <Joystick.h>
 
-#include "libs/CustomInputDebounce.h"
-#include "libs/DebouncEncoder.h"
+#include "libs/CustomInputs.h"
 
-static Joystick_ joy(0x03, JOYSTICK_TYPE_JOYSTICK, 128, 0);
+#define JOYSTICK_BASE_ADDRESS 0x0A
 
-CustomInputs::RotaryEncoder encoder1(0, 1, joy, 0, 1);
-CustomInputs::RotaryEncoder encoder2(3, 4, joy, 2, 3);
+#define JOYSTICK_COUNT 4
+Joystick_ joy[JOYSTICK_COUNT] = {
+		Joystick_(JOYSTICK_BASE_ADDRESS + 0x00, JOYSTICK_TYPE_JOYSTICK, 128, 0),
+		Joystick_(JOYSTICK_BASE_ADDRESS + 0x01, JOYSTICK_TYPE_JOYSTICK, 128, 0),
+		Joystick_(JOYSTICK_BASE_ADDRESS + 0x02, JOYSTICK_TYPE_JOYSTICK, 128, 0),
+		Joystick_(JOYSTICK_BASE_ADDRESS + 0x03, JOYSTICK_TYPE_JOYSTICK, 128, 0),
+};
+Joystick_* activeJoystick = &joy[1];
+
+void handlePress(uint8_t button) {
+	activeJoystick->pressButton(button);
+}
+void handleRelease(uint8_t button) {
+	activeJoystick->releaseButton(button);
+}
+void handleMomentaryPress(uint8_t button) {
+	handlePress(button);
+	delay(16);
+	handleRelease(button);
+	delay(1);
+}
+
+#define NUM_ROTARIES 4
+CustomInputs::RotaryEncoder rotaries[NUM_ROTARIES] = {
+		CustomInputs::RotaryEncoder(0, 1, handleMomentaryPress, 21, 22),
+		CustomInputs::RotaryEncoder(3, 4, handleMomentaryPress, 23, 24),
+		CustomInputs::RotaryEncoder(8, 9, handleMomentaryPress, 25, 26),
+		CustomInputs::RotaryEncoder(11, 12, handleMomentaryPress, 27, 28),
+};
+
+#define NUM_BUTTONS 20
+CustomInputs::Button buttons[NUM_BUTTONS] = {
+		//TOP:
+		CustomInputs::Button(34, handlePress, handleRelease, 1),
+		CustomInputs::Button(36, handlePress, handleRelease, 2),
+		CustomInputs::Button(38, handlePress, handleRelease, 3),
+		CustomInputs::Button(40, handlePress, handleRelease, 4),
+		CustomInputs::Button(42, handlePress, handleRelease, 5),
+		// RIGHT:
+		CustomInputs::Button(35, handlePress, handleRelease, 6),
+		CustomInputs::Button(37, handlePress, handleRelease, 7),
+		CustomInputs::Button(39, handlePress, handleRelease, 8),
+		CustomInputs::Button(41, handlePress, handleRelease, 9),
+		CustomInputs::Button(43, handlePress, handleRelease, 10),
+		// BOTTOM:
+		CustomInputs::Button(44, handlePress, handleRelease, 11),
+		CustomInputs::Button(46, handlePress, handleRelease, 12),
+		CustomInputs::Button(48, handlePress, handleRelease, 13),
+		CustomInputs::Button(50, handlePress, handleRelease, 14),
+		CustomInputs::Button(52, handlePress, handleRelease, 15),
+		// LEFT:
+		CustomInputs::Button(45, handlePress, handleRelease, 16),
+		CustomInputs::Button(47, handlePress, handleRelease, 17),
+		CustomInputs::Button(49, handlePress, handleRelease, 18),
+		CustomInputs::Button(51, handlePress, handleRelease, 19),
+		CustomInputs::Button(53, handlePress, handleRelease, 20),
+};
 
 void setup() {
-	Serial.begin(115200);
-	joy.begin();
+	// Serial.begin(250000);
+	for (int j = 0; j < JOYSTICK_COUNT; j++) {
+		joy[j].begin();
+	}
 }
 
 void loop() {
+	for (int r = 0; r < NUM_ROTARIES; r++) {
+		rotaries[r].pollInput();
+	}
+
 	unsigned long now = millis();
-	encoder1.pollInput();
-	encoder2.pollInput();
+	for (int b = 0; b < NUM_BUTTONS; b++) {
+		buttons[b].process(now);
+	}
 }
